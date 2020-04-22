b5b72d435b00ff93152779caf1d291454ab220c8
furandon-pig <furandon.pig@gmail.com>
2020-04-22 19:39:37 +0900

    「陽性患者数」「陽性患者の属性」カードにオープンデータへのリンクを追加 しました。

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -7,6 +7,7 @@
       :chart-data="patientsGraph"
       :date="PatientsSummary.date"
       :unit="$t('人')"
+      :url="'http://www.okayama-opendata.jp/opendata/ga120PreAction.action?keyTitle=d9c4776db7f09fff161953a2aaf03b80a9abad48&datasetId=e6b3c1d2-2f1f-4735-b36e-e45d36d94761'"
     />
   </v-col>
 </template>
