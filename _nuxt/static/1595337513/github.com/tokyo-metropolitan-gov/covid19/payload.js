719ca85f175a2f86571970523b03d87ba964472c
fujiwaraizuho <fujiwaraizuho@users.noreply.github.com>
2020-07-21 13:18:39 +0000

    deploy: 1a2e5b29fd371dbb4057c24f32c3b6104f668b59

---
+++ _nuxt/static/1595337513/github.com/tokyo-metropolitan-gov/covid19/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("//github.com/tokyo-metropolitan-gov/covid19", {data:[],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1595337513/medical/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/medical", {data:[{}],fetch:[],mutations:void 0});
