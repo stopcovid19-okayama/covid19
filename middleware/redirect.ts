15a8496287d58c4b42aa7e446fb12a328ba7437d
Izuho Fujiwara <contact@fujishan.jp>
2020-12-28 23:56:32 +0900

    Fix: emergency fix

--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -1,13 +1,7 @@
-/*
 import { Middleware } from '@nuxt/types'
 
-const redirect: Middleware = ({ route, redirect }) => {
-  if (route.path.includes('flow')) {
-    redirect(
-      'https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html'
-    )
-  }
+const redirect: Middleware = () => {
+  //
 }
 
 export default redirect
-*/
